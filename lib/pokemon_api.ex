395f04229935b14ea70858028ef370e7008c7a52
pedrohsampaioo <pedrohsampaioo@github.com>
2021-01-27 21:44:43 -0300

    delegate for trainer CRUD actions

--- lib/pokemon_api.ex
+++ lib/pokemon_api.ex
@@ -1,15 +1,7 @@
 defmodule PokemonApi do
   alias PokemonApi.Trainer
-
-  @moduledoc """
-  PokemonApi keeps the contexts that define your domain
-  and business logic.
-
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
-
   defdelegate create_trainer(params), to: Trainer.Create, as: :call
-
-  defdelegate delete_trainer(params), to: Trainer.Delete, as: :call
+  defdelegate delete_trainer(uuid), to: Trainer.Delete, as: :call
+  defdelegate get_trainer(uuid), to: Trainer.Get, as: :call
+  defdelegate update_trainer(params), to: Trainer.Update, as: :call
 end
