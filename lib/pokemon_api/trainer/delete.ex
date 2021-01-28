0e79ff679946626da0e50419cf526f984dbb6e17
pedrohsampaioo <pedrohsampaioo@github.com>
2021-01-27 21:41:25 -0300

    Implementation of the delete trainer function

--- lib/pokemon_api/trainer/delete.ex
+++ lib/pokemon_api/trainer/delete.ex
@@ -2,22 +2,14 @@ defmodule PokemonApi.Trainer.Delete do
   alias Ecto.UUID
   alias PokemonApi.{Repo, Trainer}
 
-  def call(id) do
-    id
-    |> uuid_valid?()
+  def call(uuid) do
+    case UUID.cast(uuid) do
+      :error -> {:error, "It's not a valid UUID"}
+      {:ok, _uuid} -> delete(uuid)
+    end
   end
 
-  defp uuid_valid?(uuid) do
-    uuid
-    |> UUID.cast()
-    |> handle_uuid_cast_result()
-  end
-
-  defp handle_uuid_cast_result(:error), do: {:error, "It is not a valid UUID"}
-
-  defp handle_uuid_cast_result({:ok, uuid}), do: delete_trainer(uuid)
-
-  def delete_trainer(uuid) do
+  defp delete(uuid) do
     case Repo.get(Trainer, uuid) do
       nil -> {:error, "Trainer not found"}
       trainer -> Repo.delete(trainer)
