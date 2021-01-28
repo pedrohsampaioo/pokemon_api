d7287f1fa6f030c529c71427634d867d1ea93825
pedrohsampaioo <pedrohsampaioo@github.com>
2021-01-27 21:46:27 -0300

    Trainer controller implementation

--- lib/pokemon_api_web/controllers/trainers_controller.ex
+++ lib/pokemon_api_web/controllers/trainers_controller.ex
@@ -1,19 +1,46 @@
-defmodule PokemonApiWeb.TrainersController do
-  use PokemonApiWeb, :controller
+defmodule PokemonApi.TrainersController do
+  use PokemonApi, :controller
 
-  action_fallback PokemonApiWeb.FallbackController
+  action_fallback PokemonApi.FallbackController
+
+  def show(conn, %{"id" => uuid}) do
+    uuid
+    |> ExMon.get_trainer()
+    |> handle_response(conn, :ok, "get_trainer.json")
+  end
 
   def create(conn, params) do
     params
-    |> PokemonApi.create_trainer()
-    |> handle_response(conn)
+    |> ExMon.create_trainer()
+    |> handle_response(conn, :created, "create_trainer.json")
   end
 
-  defp handle_response({:ok, trainer}, conn) do
+  def update(conn, params) do
+    params
+    |> ExMon.update_trainer()
+    |> handle_response(conn, :ok, "update_trainer.json")
+  end
+
+  def delete(conn, %{"id" => uuid}) do
+    uuid
+    |> ExMon.delete_trainer()
+    |> handle_delete_response(conn)
+  end
+
+  defp handle_response({:ok, trainer}, conn, status, render_name) do
     conn
-    |> put_status(:created)
-    |> render("create.json", trainer: trainer)
+    |> put_status(status)
+    |> render(render_name, trainer: trainer)
   end
 
-  defp handle_response({:error, _changeset} = error, _conn), do: error
+  defp handle_response({:error, _changeset_or_reason} = error, _conn, _status, _render_name),
+    do: error
+
+  defp handle_delete_response({:ok, _trainer}, conn) do
+    conn
+    |> put_status(:no_content)
+    |> text("")
+  end
+
+  defp handle_delete_response({:error, _reason} = error, _conn), do: error
 end
